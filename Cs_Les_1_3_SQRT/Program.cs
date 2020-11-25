f312bea90f63024ef423261e612336a2785243a9
Vladimir <weizer@mail.ru>
2020-11-25 15:52:01 +0300

    Лямбда

--- Cs_Les_1_3_SQRT/Program.cs
+++ Cs_Les_1_3_SQRT/Program.cs
@@ -4,6 +4,7 @@ namespace Cs_Les_1_3_SQRT
 {
     class Program
     {
+        delegate double Coordinate_2(int _x1, int _y1, int _x2, int _y2);
         static void Main(string[] args)
         {
             //Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2).
@@ -31,12 +32,18 @@ namespace Cs_Les_1_3_SQRT
             //б) *Выполните предыдущее задание, оформив вычисления расстояния между точками в виде метода;
             Console.WriteLine(Coordinate(x1, y1, x2, y2).ToString("f2"));
 
+            //б2) *Выполните предыдущее задание, оформив вычисления расстояния между точками в виде метода;
+            Coordinate_2 coordinate_2 = (_x1, _y1, _x2, _y2) => Math.Sqrt(Math.Pow(_x2 - _x1, 2) + Math.Pow(_y2 - _y1, 2));
+            Console.WriteLine("\n");
+            Console.WriteLine(coordinate_2(x1, y1, x2, y2).ToString("f2"));
+
             Console.ReadKey();
             //TODO Доделать в метод
-            double Coordinate(int x1f, int y1f, int x2f, int y2f)
+            double Coordinate(int _x1, int _y1, int _x2, int _y2)
             {
-                return Math.Sqrt(Math.Pow(x2f - x1f, 2) + Math.Pow(y2f - y1f, 2));
+                return Math.Sqrt(Math.Pow(_x2 - _x1, 2) + Math.Pow(_y2 - _y1, 2));
             }
+
 
 
         }
